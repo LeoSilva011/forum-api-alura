8397fbafac6844e2818f684a665166f6b4ad34ac
Leo <leonardosilva01107@gmail.com>
2022-08-13 14:46:51 -0300

    sdsd

--- src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDto.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 import br.com.alura.forum.modelo.StatusTopico;
 import br.com.alura.forum.modelo.Topico;
-//iiii
+//iiiisdsd
 public class DetalhesDoTopicoDto {
 	
 	private Long id;
